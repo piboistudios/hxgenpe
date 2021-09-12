dd9a9b04f4d2ae42a8029dbd836c44a9e1e59552
Gabriel Hayes <gabriel.speed.bullock@gmail.com>
2021-09-12 14:22:29 -0500

    Conversion from hscript.TTYpe to BaseTypeRef

--- src/haxe/compiler/backends/GenPE.hx
+++ src/haxe/compiler/backends/GenPE.hx
@@ -1,5 +1,14 @@
 package haxe.compiler.backends;
 
+import mono.ilasm.MethodRef;
+import mono.ilasm.BaseClassRef;
+import mono.ilasm.ExternTypeRef;
+import mono.ilasm.GenericArguments;
+import haxe.exceptions.NotImplementedException;
+import mono.ilasm.BaseGenericTypeRef;
+import cs.system.reflection.SignatureGenericParameterType;
+import peapi.PrimitiveType;
+import mono.ilasm.PrimitiveTypeRef;
 import mono.ilasm.TypeDef;
 
 using hscript.Tools;
@@ -40,18 +49,22 @@ using haxe.compiler.backends.GenPE;
 class GenPE {
 	public var gen:CodeGen;
 	public var types:PECheckerTypes;
-    var outputFile = '';
-	var currentPackage = '';
 
+	var outputFile = '';
+	var currentPackage = '';
+	// I guess we shouldn't force dependencies... but for now, this will rely on System.Linq.Expressions.dll
+	var useSystemDynamicTypes = true;
 	var evaluatedStaticSets:Map<String, Map<String, Dynamic>> = [];
 	var deferred:Array<Void->Void> = [];
 	var interp = new Interp();
 
 	public function new(outputFile, isDll, debuggingInfo, autoInherit) {
 		this.gen = new CodeGen(outputFile, isDll, debuggingInfo, autoInherit);
-        this.outputFile = outputFile;
+		this.outputFile = outputFile;
 	}
 
+	// pretty sure you won't be able to support complex static initializers
+	// because eval doesn't exist in Haxe... I guess unless you run haxe and interpret stuff..
 	public function buildHaxe(types:Array<haxe.macro.Type>) {
 		throw 'not implemented';
 	}
@@ -102,12 +115,12 @@ class GenPE {
 	}
 
 	// run deferred tasks (finish prepping codegen) and generate PE
-	// I think, if this isn't clumsy you shouldn't need types but.. 
-    // what the heck, more info the better I guess..
+	// I think, if this isn't clumsy you shouldn't need types but..
+	// what the heck, more info the better I guess..
 	function finalPass(types:Array<hscript.Expr.ModuleDecl>) {
 		for (deferred in deferred)
 			deferred();
-        gen.Write(); // assemblies have been outputted
+		gen.Write(); // assemblies have been outputted
 	}
 
 	function generateMethod(owner:String, field:FieldDecl, f:FunctionDecl) {
@@ -146,18 +159,65 @@ class GenPE {
 
 	var mainClass(default, null):String;
 
-    // this is kind of dumb, really only exists to some interface I made up for haxe compiler back ends..
-    // technically back-ends return binaries directly.. I think that's the right way to go about this
-    // if you just want the file, you can ignore the output I guess, but I imagine if you're
-    // generating binaries and intending to send them somewhere, this I will handle reading the file for you
-    // hm... opinions.. decisions..
+	// this is kind of dumb, really only exists to some interface I made up for haxe compiler back ends..
+	// technically back-ends return binaries directly.. I think that's the right way to go about this
+	// if you just want the file, you can ignore the output I guess, but I imagine if you're
+	// generating binaries and intending to send them somewhere, this I will handle reading the file for you
+	// hm... opinions.. decisions..
 	function getBinary():Bytes {
 		return sys.io.File.getBytes(outputFile);
 	}
 
-	function toClrTypeRef(ret:TType):BaseTypeRef {
-		throw new haxe.exceptions.NotImplementedException();
-	}
+	public function clrApply(e:BaseClassRef, types:Array<TType>)
+		return e.GetGenericTypeInst({
+			var args = new GenericArguments();
+			for (t in types)
+				args.Add(toClrTypeRef(t));
+			args;
+		});
+
+	function toClrTypeRef(t:TType):BaseTypeRef
+		return switch types.checker.follow(t) {
+			case TVoid: new PrimitiveTypeRef(PrimitiveType.Void, "System.Void");
+			case TInt: new PrimitiveTypeRef(PrimitiveType.Int32, "System.Int32");
+			case TFloat: new PrimitiveTypeRef(PrimitiveType.Float32, "System.Single");
+			case TBool: new PrimitiveTypeRef(PrimitiveType.Boolean, "System.Boolean");
+			case TDynamic: getDynamicTypeRef();
+			case TAbstract(a, args): toClrTypeRef(types.checker.apply(a.t, a.params, args));
+			case TInst(c, args): clrApply(gen.GetTypeRef(c.name), args);
+			case TNull(t): clrApply(gen.ExternTable.GetTypeRef("mscorlib.dll", "System.Nullable", false), [t]);
+            case TEnum(e, args): clrApply(gen.GetTypeRef(e.name), args) ;
+            case TType(t, args): clrApply(gen.GetTypeRef(t.name), args);
+            case TFun(args, ret): clrApply(getFunctionTypeRef(args, ret), [for(arg in args) arg.t]); // These need to capture generics?
+            case TAnon(fields): getDynamicTypeRef();
+            default:
+                throw 'Error, invalid type: $t';
+			case TParam(name):
+				/*
+					This is what needs to happen for TParam
+					From ILParser.jay...
+					For generics in the current type definition's signature:
+						int num = -1;
+						string name = (string) $2;
+						if (codegen.CurrentTypeDef != null)
+							num = codegen.CurrentTypeDef.GetGenericParamNum (name);
+						GenParam gpar = new GenParam (num, name, GenParamType.Var);
+										$$ = new GenericParamRef (gpar, name);
+					For generics in the current method definition's signature:
+						int num = -1;
+						string name = (string) $3;
+						if (codegen.CurrentMethodDef != null)
+							num = codegen.CurrentMethodDef.GetGenericParamNum (name);
+						GenParam gpar = new GenParam (num, name, GenParamType.MVar);
+										$$ = new GenericParamRef (gpar, name);
+						To do this, we'd need to capture:
+							- Whether or not we're in a type def (error otherwise :) )
+							- Whether or not we're in a method body
+							- A list of the type parameters belonging to the current class
+							- A list of the type parameters belonging to the current method (if we're in one)
+				 */
+				throw new NotImplementedException();
+		}
 
 	/**
 	 * Gets the CLR flags for a hscript field decl
@@ -180,6 +240,14 @@ class GenPE {
 	}
 
 	function tryInitField(field:FieldDef, typeDef:TypeDef) {}
+
+	function getDynamicTypeRef():BaseTypeRef {
+		throw new haxe.exceptions.NotImplementedException();
+	}
+
+	function getFunctionTypeRef(args:Array<{name:String, opt:Bool, t:TType}>, ret:TType):BaseClassRef {
+		throw new haxe.exceptions.NotImplementedException();
+	}
 }
 
 class ClrMethodDefTools {
